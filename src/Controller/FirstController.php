a0f623c03d776d320970d7c3f308c225d95df26e
LH <lh@applh.com>
2021-02-10 17:02:47 +0100

    commit test de pages

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -11,8 +11,25 @@ class FirstController extends AbstractController
     #[Route('/first', name: 'first')]
     public function index(): Response
     {
+        return $this->render('first/first.html.twig', [
+            'controller_name' => 'FirstController',
+        ]);
+    }
+
+    #[Route('/', name: 'accueil')]
+    public function accueil(): Response
+    {
         return $this->render('first/index.html.twig', [
             'controller_name' => 'FirstController',
         ]);
     }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('first/contact.html.twig', [
+            'controller_name' => 'FirstController',
+        ]);
+    }
+
 }
