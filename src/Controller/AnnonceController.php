c0f6f7a931574a34e194e6ff1c39ed83b167607c
LH <lh@applh.com>
2021-02-16 09:42:19 +0100

    ajout prefixe /admin sur les routes Controller

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/annonce')]
+#[Route('/admin/annonce')]
 class AnnonceController extends AbstractController
 {
     #[Route('/', name: 'annonce_index', methods: ['GET'])]
--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/categorie')]
+#[Route('/admin/categorie')]
 class CategorieController extends AbstractController
 {
     #[Route('/', name: 'categorie_index', methods: ['GET'])]
