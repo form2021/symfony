eff94aaff15017dcace5e74ced661e6f8b81d450
LH <lh@applh.com>
2021-02-17 12:58:00 +0100

    ajout code upload image

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -10,11 +10,13 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Annonce;
 use App\Form\AnnonceType;
 use App\Repository\AnnonceRepository;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class MembreController extends AbstractController
 {
     #[Route('/membre', name: 'membre', methods: ['GET', 'POST'])]
-    public function index(Request $request, AnnonceRepository $annonceRepository): Response
+    public function index(Request $request, AnnonceRepository $annonceRepository, SluggerInterface $slugger): Response
     {
         $annonce = new Annonce();
         $form = $this->createForm(AnnonceType::class, $annonce);
@@ -29,6 +31,35 @@ class MembreController extends AbstractController
             // ajouter l'auteur de l'annonce avec l'utilisateur connecté
             $annonce->setUser($userConnecte);
             
+            // code de gestion de upload image
+            $imageFile = $form->get('image')->getData();
+
+            // this condition is needed because the 'image' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                // Move the file to the directory where images are stored
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),    // dossier cible
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'imageFilename' property to store the PDF file name
+                // instead of its contents
+                $annonce->setImage($newFilename);
+            }
+            else {
+                $annonce->setImage("");     // aucun fichier uploade
+            }
+
             // code qui insère la nouvelle ligne dans SQL
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($annonce);
--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -6,6 +6,10 @@ use App\Entity\Annonce;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+// ne pas oublier d'ajouter les use
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+
 
 class AnnonceType extends AbstractType
 {
@@ -15,7 +19,27 @@ class AnnonceType extends AbstractType
             ->add('titre')
             ->add('slug')
             ->add('contenu')
-            ->add('image')
+            ->add('image', FileType::class, [
+                'label' => 'choisissez une photo à uploader',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '10240k',
+                        'mimeTypes' => [
+                        ],
+                        'mimeTypesMessage' => 'type de fichier incorrect',
+                    ])
+                ],
+            ])
             // ->add('datePublication')
         ;
     }
