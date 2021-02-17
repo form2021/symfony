5982df8e704039951f683afc396c0d14c8df891b
LH <lh@applh.com>
2021-02-17 15:14:37 +0100

    ajout update annonce espace membre

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -82,7 +82,7 @@ class MembreController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'membre_annonce_delete', methods: ['DELETE'])]
+    #[Route('/membre/{id}', name: 'membre_annonce_delete', methods: ['DELETE'])]
     public function delete(Request $request, Annonce $annonce): Response
     {
         if ($this->isCsrfTokenValid('delete'.$annonce->getId(), $request->request->get('_token'))) {
@@ -109,6 +109,73 @@ class MembreController extends AbstractController
         }
 
         return $this->redirectToRoute('membre');
+    }
+
+    #[Route('membre/{id}/edit', name: 'membre_annonce_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Annonce $annonce, SluggerInterface $slugger): Response
+    {
+        $userConnecte = $this->getUser();
+        $auteurAnnonce = $annonce->getUser();
+        if ($userConnecte == null || $auteurAnnonce == null) {
+            // erreur renvoyer vers l'espace membre
+            return $this->redirectToRoute('membre');
+        }
+        else if ($userConnecte->getId() != $auteurAnnonce->getId()) {
+            // erreur renvoyer vers l'espace membre
+            return $this->redirectToRoute('membre');
+        }
+
+        // ok les auteurs correspondent et on peut continuer
+        $form = $this->createForm(AnnonceType::class, $annonce);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // code de gestion de upload image
+            $imageFile = $form->get('image')->getData();
+
+            // this condition is needed because the 'image' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename  = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                // Move the file to the directory where images are stored
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),    // dossier cible
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // supprimer l'image d'avant
+                $dossierUpload = $this->getParameter('images_directory');
+                $fichierImage = "$dossierUpload/" . $annonce->getImage();
+                if (is_file($fichierImage)) {
+                    unlink($fichierImage);
+                }
+
+                // updates the 'imageFilename' property to store the PDF file name
+                // instead of its contents
+                $annonce->setImage($newFilename);
+            }
+            else {
+                // on laisse l'image d'avant => on ne fait rien
+            }
+
+            // la mise est à jour est déclenchée automatiquement 
+            // car Symfony sait déjà que l'objet $annonce est associée à une ligne SQL
+            $this->getDoctrine()->getManager()->flush();
+
+        }
+
+        return $this->render('membre/edit.html.twig', [
+            'annonce' => $annonce,
+            'form' => $form->createView(),
+        ]);
     }
 
 }
