5b265e5175c76abc814e9f4dab2df2b5d1f1d864
LH <lh@applh.com>
2021-02-17 12:07:28 +0100

    ajout affichage annonces filtre dans espace membre

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -9,23 +9,24 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Annonce;
 use App\Form\AnnonceType;
+use App\Repository\AnnonceRepository;
 
 class MembreController extends AbstractController
 {
     #[Route('/membre', name: 'membre', methods: ['GET', 'POST'])]
-    public function index(Request $request): Response
+    public function index(Request $request, AnnonceRepository $annonceRepository): Response
     {
         $annonce = new Annonce();
         $form = $this->createForm(AnnonceType::class, $annonce);
         $form->handleRequest($request);
 
+        $userConnecte = $this->getUser();
         $messageConfirmation = "";
         if ($form->isSubmitted() && $form->isValid()) {
             // compléter les infos manquantes
             $annonce->setDatePublication(new \DateTime());
             // https://symfony.com/doc/current/security.html#a-fetching-the-user-object
             // ajouter l'auteur de l'annonce avec l'utilisateur connecté
-            $userConnecte = $this->getUser();
             $annonce->setUser($userConnecte);
             
             // code qui insère la nouvelle ligne dans SQL
@@ -36,7 +37,14 @@ class MembreController extends AbstractController
             $messageConfirmation = "votre annonce est publiée";
         }
 
+        // après le traitement du create pour obtenir la liste à jour
+        $annonces = $annonceRepository->findBy([
+            "user" => $userConnecte,            
+            // on filtre les lignes pour obtenir seulement les annonces de l'utilisateur
+        ], [ "datePublication" => "DESC"]);
+
         return $this->render('membre/index.html.twig', [
+            'annonces' => $annonces,    // LISTE DES ANNONCES
             'annonce' => $annonce,
             'form' => $form->createView(),
             'messageConfirmation' => $messageConfirmation,
